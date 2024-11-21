0d322c9bed96b692f174af6a977c28cae8528c1b
m0r4a <m0r4a@proton.me>
2024-11-21 14:35:29 -0600

    Make rofi look not atrocious

--- packages/scripts/scripts/WallpaperSelect.sh
+++ packages/scripts/scripts/WallpaperSelect.sh
@@ -6,24 +6,33 @@ wallDIR="$HOME/Wallpapers/"
 # Variables
 focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
 
-# Retrieve image files using null delimiter to handle spaces in filenames
+CURRENT_WALLPAPER_PATH=$(grep -m 1 '^wallpaper=' "$HOME/.cache/wal/colors.sh" | cut -d '=' -f 2 | tr -d '"')
+
+# Getting the images
 mapfile -d '' PICS < <(find "${wallDIR}" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" \) -print0)
 
-# Random picture
-RANDOM_PIC="${PICS[$((RANDOM % ${#PICS[@]}))]}"
-RANDOM_PIC_NAME=". random"
+# Temp value for random_pic
+RANDOM_PIC=""
+
+# Loop until the random pic isn't the same as the current one
+while [[ "$RANDOM_PIC" == "$CURRENT_WALLPAPER_PATH" || -z "$RANDOM_PIC" ]]; do
+    RANDOM_PIC="${PICS[$((RANDOM % ${#PICS[@]}))]}"
+done
+
+RANDOM_PIC_NAME="random"
 
 # Rofi command
 rofi_command="rofi -i -show -dmenu -config ~/.config/rofi/config-wallpaper.rasi"
 
 # Function to display the wallpaper menu
 menu() {
-  printf "%s\x00icon\x1f%s\n" "$RANDOM_PIC_NAME" "$RANDOM_PIC"
-  
-  # Sort and format options
+  formatted_name=$(echo "$RANDOM_PIC_NAME" | sed 's/_/ /g')
+  printf "%s\x00icon\x1f%s\n" "$formatted_name" "$RANDOM_PIC"
+
   for pic_path in $(printf "%s\n" "${PICS[@]}" | sort); do
     pic_name=$(basename "$pic_path")
-    printf "%s\x00icon\x1f%s\n" "$(echo "$pic_name" | cut -d. -f1)" "$pic_path"
+    formatted_pic_name=$(echo "$pic_name" | cut -d. -f1 | sed 's/_/ /g')
+    printf "%s\x00icon\x1f%s\n" "$formatted_pic_name" "$pic_path"
   done
 }
 
@@ -48,6 +57,8 @@ EOF
 # Main function to handle wallpaper selection
 main() {
   local choice=$(menu | $rofi_command | xargs) # Trim whitespace
+
+  choice=$(echo $choice | sed 's/ /_/g')
 
   if [[ -z "$choice" ]]; then
     echo "No choice selected. Exiting."
